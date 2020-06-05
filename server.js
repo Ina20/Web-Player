e84064752a8e2b2a44f4e231a0fa50e34ed1a5bc
Ina20 <inaszkola@gmail.com>
2020-06-05 13:26:29 +0200

    some CSS

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
 app.use(express.static(__dirname + '/scripts'));
-app.use(express.static(__dirname + '/styles'));
+app.use('/styles', express.static(__dirname + '/styles'));
+app.use('/images', express.static(__dirname + '/images'));
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 app.listen(port, () => console.log("App listening at http://localhost:3000"));
